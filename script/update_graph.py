25128a4b19a167f065be11e9275acd532c5d0582
coolYang <yangforbig@gmail.com>
2024-10-22 18:42:16 -0700

    added MA

--- script/update_graph.py
+++ script/update_graph.py
@@ -44,7 +44,8 @@ def get_historical_price_data(symbol, start_date, end_date, api_key):
     df = pd.DataFrame(data)
     df['date'] = pd.to_datetime(df['time_period_start']).dt.date
     df = df.rename(columns={'rate_open': 'daily_min_price', 'rate_high': 'daily_max_price', 'rate_close': 'daily_avg_price'})
-    return df[['date', 'daily_min_price', 'daily_max_price', 'daily_avg_price']]
+    df['MA_125'] = df['daily_avg_price'].rolling(window=125).mean()
+    return df[['date', 'daily_min_price', 'daily_max_price', 'daily_avg_price','MA_125']]
 
 
 # Replace with your actual CoinAPI key
@@ -60,9 +61,13 @@ def get_bitcoin_price_data(days=30):
     start_date = end_date - timedelta(days=days)
     return get_historical_price_data("BTC", start_date, end_date, api_key)
 
+
 def plot_data_with_fear_greed_alerts(merged_df, symbol):
     # Sort the dataframe by date
     merged_df = merged_df.sort_values('date')
+
+    # Calculate the 125-day moving average
+    merged_df['MA_125'] = merged_df['daily_avg_price'].rolling(window=125).mean()
 
     # Create figure with secondary y-axis
     fig = make_subplots(specs=[[{"secondary_y": True}]])
@@ -77,6 +82,12 @@ def plot_data_with_fear_greed_alerts(merged_df, symbol):
     fig.add_trace(
         go.Scatter(x=merged_df['date'], y=merged_df['value'], name="Fear & Greed Index", line=dict(color='black', width=2)),
         secondary_y=True,
+    )
+
+    # Add 125-day Moving Average trace
+    fig.add_trace(
+        go.Scatter(x=merged_df['date'], y=merged_df['MA_125'], name="MA (125)", line=dict(color='purple', width=2, dash='dash')),
+        secondary_y=False,
     )
 
     # Add colored background for different index ranges
@@ -112,7 +123,7 @@ def plot_data_with_fear_greed_alerts(merged_df, symbol):
 
     return fig
 
-time_period = 730  # 2 years
+time_period = 365 * 6  # 2 years
 fear_greed_df = get_fear_greed_data(time_period)
 eth_df = get_eth_price_data(time_period)
 bitcoin_df = get_bitcoin_price_data(time_period)
